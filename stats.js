3c820121386af0c32f0dcf82451d70d3862b7d9f
yadavalu <86847198+yadavalu@users.noreply.github.com>
2025-06-10 03:50:51 +0000

    Added expansion

--- stats.js
+++ stats.js
@@ -3,20 +3,20 @@ const no_of_panels = document.getElementById('no_panels');
   new Chart(no_of_panels, {
     type: 'doughnut',
     data: {
-      labels: ['Berlin', 'Chennai', 'Johannesburg'],
+      labels: ['Berlin', 'Chennai'],
       datasets: [{
         label: 'Number of panels',
-        data: [44, 14, 3],
+        data: [44, 14],
         borderWidth: 1,
         backgroundColor: [
             'rgba(255, 99, 132, 0.5)',
             'rgba(54, 162, 235, 0.5)',
-            'rgba(75, 192, 192, 0.5)',
+            //'rgba(75, 192, 192, 0.5)',
         ],
         borderColor: [
             'rgba(255, 99, 132, 1)',
             'rgba(54, 162, 235, 1)',
-            'rgba(75, 192, 192, 1)',
+            //'rgba(75, 192, 192, 1)',
         ],
       }]
     },
@@ -27,20 +27,20 @@ const power_output = document.getElementById('power_output');
   new Chart(power_output, {
     type: 'bar',
     data: {
-      labels: ['Berlin', 'Chennai', 'Johannesburg'],
+      labels: ['Berlin', 'Chennai'],
       datasets: [{
         label: 'Approx Maximum Power Output [Watts]',
-        data: [18000, 5800, 1250],
+        data: [18000, 5800],
         borderWidth: 1,
         backgroundColor: [
             'rgba(255, 99, 132, 0.5)',
             'rgba(54, 162, 235, 0.5)',
-            'rgba(75, 192, 192, 0.5)',
+            //'rgba(75, 192, 192, 0.5)',
         ],
         borderColor: [
             'rgba(255, 99, 132, 1)',
             'rgba(54, 162, 235, 1)',
-            'rgba(75, 192, 192, 1)',
+            // 'rgba(75, 192, 192, 1)',
         ],
       }]
     },
